7ce6e2bd68f261517b5a7fe0c70300b29e88d8b3
ggoraa <ggoraa1029@gmail.com>
2021-04-26 21:50:40 +0300

    fixed makescript

--- makescripts/check-deployment.sh
+++ makescripts/check-deployment.sh
@@ -1,4 +1,4 @@
-if [ -f /Volumes/GARMIN/GARMIN/APPS/WheelLogGarminApp.prg ]; then
+if [ -f /Volumes/GARMIN/GARMIN/APPS/WheelLogCompanionApp.prg ]; then
     echo "App exists on end device, ready for use!"
 else 
     echo "App does not exist, you need to deploy it."
